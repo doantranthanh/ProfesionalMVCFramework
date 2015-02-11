e13c2e7cb8135dcf65f697d592a4de3368089e89
Thanh Doan <T.Doan@xlntelecom.co.uk>
2015-02-11 16:47:11 +0000

    adding cart line

---
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ninject;
+using SportsStore.Domain.Abastract;
+using SportsStore.Domain.Concrete;
+using SportsStore.WebUI.Infrastructure.Abstract;
+using SportsStore.WebUI.Infrastructure.Concrete;
+using System.Configuration;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+    }
+}
